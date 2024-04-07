e8405741bd035edb3788ffe28f5454938afeb3d5
Ant0nYurchenk0 <an.yurchenko.an@gmail.com>
2024-04-07 13:35:30 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/Prog/C++/Refactoring-Labs/Lab2_SaveR
+# Build directory: /mnt/d/Prog/C++/Refactoring-Labs/Lab2_SaveR/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
